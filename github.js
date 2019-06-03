0e72b933b641e3dafc8490565196540f009d68f7
Marco Antonio Loyo <marco.loyo@bigcommerce.com>
2019-06-03 16:11:30 -0500

    refactor(service): rename variables and way to declare them

--- github.js
+++ github.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-const Octokit = require('@octokit/rest');
-const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
-});
+const Octokit = require('@octokit/rest'),
+    octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN
+    });
 
 
 module.exports = {
-    listPRs: (orgs, authors) => {
+    getPRs: (orgs, authors) => {
         const q = `is:pr+is:open+user:${orgs.join('+user:')}+author:${authors.join('+author:')}`;
 
         return octokit.search.issuesAndPullRequests({ q });
